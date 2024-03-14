0cea7b2b0bc2885868a9a78591e8fff417f47438
PramodY2802 <pramodyadav3142@gmail.com>
2024-03-14 18:20:12 +0530

    changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT=process.env.PORT || 8000;
 const app =express();
 app.use(express.json());
 app.use(cors({
-    origin:["https://ttmm-services.netlify.app/"],
+    origin:["https://ttmm-services.netlify.app"],
     credentials:true
 }));
 
